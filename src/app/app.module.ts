3a2e57aec9f84f0641aef86d7092f6c1c198f2a9
ProfZidane <madazada0@gmail.com>
2024-04-15 03:18:13 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { DockModule } from 'primeng/dock';
+import { MenubarModule } from 'primeng/menubar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DialogModule } from 'primeng/dialog';
+import { TerminalModule } from 'primeng/terminal';
+import { TerminalService } from 'primeng/terminal';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    DockModule,
+    MenubarModule,
+    DialogModule, 
+    TerminalModule
   ],
-  providers: [],
+  providers: [TerminalService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,188 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem, MessageService } from 'primeng/api';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  displayTerminal: boolean | any;
+  items: MenuItem[] | any;
+  menubarItems: any[] | any;
+  position: string = 'bottom';
+
+  positionOptions = [
+      {
+          label: 'Bottom',
+          value: 'bottom'
+      },
+      {
+          label: 'Top',
+          value: 'top'
+      },
+      {
+          label: 'Left',
+          value: 'left'
+      },
+      {
+          label: 'Right',
+          value: 'right'
+      }
+  ];
+
+  
+  constructor() { }
+
+  ngOnInit(): void {
+    this.items = [
+        {
+            label: 'Finder',
+            icon: 'https://primefaces.org/cdn/primeng/images/dock/finder.svg'
+        },
+        {
+            label: 'Terminal',
+            icon: 'https://primefaces.org/cdn/primeng/images/dock/terminal.svg',
+            command: () => {
+                this.displayTerminal = true;
+            }
+        },
+        {
+            label: 'App Store',
+            icon: 'https://primefaces.org/cdn/primeng/images/dock/appstore.svg'
+        },
+        {
+            label: 'Photos',
+            icon: 'https://primefaces.org/cdn/primeng/images/dock/photos.svg'
+        },
+        {
+            label: 'Trash',
+            icon: 'https://primefaces.org/cdn/primeng/images/dock/trash.png'
+        }
+    ];
+
+    this.menubarItems = [
+        {
+            label: 'Finder',
+            styleClass: 'menubar-root'
+        },
+        {
+            label: 'File',
+            items: [
+                {
+                    label: 'New',
+                    icon: 'pi pi-fw pi-plus',
+                    items: [
+                        {
+                            label: 'Bookmark',
+                            icon: 'pi pi-fw pi-bookmark'
+                        },
+                        {
+                            label: 'Video',
+                            icon: 'pi pi-fw pi-video'
+                        }
+                    ]
+                },
+                {
+                    label: 'Delete',
+                    icon: 'pi pi-fw pi-trash'
+                },
+                {
+                    separator: true
+                },
+                {
+                    label: 'Export',
+                    icon: 'pi pi-fw pi-external-link'
+                }
+            ]
+        },
+        {
+            label: 'Edit',
+            items: [
+                {
+                    label: 'Left',
+                    icon: 'pi pi-fw pi-align-left'
+                },
+                {
+                    label: 'Right',
+                    icon: 'pi pi-fw pi-align-right'
+                },
+                {
+                    label: 'Center',
+                    icon: 'pi pi-fw pi-align-center'
+                },
+                {
+                    label: 'Justify',
+                    icon: 'pi pi-fw pi-align-justify'
+                }
+            ]
+        },
+        {
+            label: 'Users',
+            items: [
+                {
+                    label: 'New',
+                    icon: 'pi pi-fw pi-user-plus'
+                },
+                {
+                    label: 'Delete',
+                    icon: 'pi pi-fw pi-user-minus'
+                },
+                {
+                    label: 'Search',
+                    icon: 'pi pi-fw pi-users',
+                    items: [
+                        {
+                            label: 'Filter',
+                            icon: 'pi pi-fw pi-filter',
+                            items: [
+                                {
+                                    label: 'Print',
+                                    icon: 'pi pi-fw pi-print'
+                                }
+                            ]
+                        },
+                        {
+                            icon: 'pi pi-fw pi-bars',
+                            label: 'List'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            label: 'Events',
+            items: [
+                {
+                    label: 'Edit',
+                    icon: 'pi pi-fw pi-pencil',
+                    items: [
+                        {
+                            label: 'Save',
+                            icon: 'pi pi-fw pi-calendar-plus'
+                        },
+                        {
+                            label: 'Delete',
+                            icon: 'pi pi-fw pi-calendar-minus'
+                        }
+                    ]
+                },
+                {
+                    label: 'Archieve',
+                    icon: 'pi pi-fw pi-calendar-times',
+                    items: [
+                        {
+                            label: 'Remove',
+                            icon: 'pi pi-fw pi-calendar-minus'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            label: 'Quit'
+        }
+    ];
+  }
+
+}
